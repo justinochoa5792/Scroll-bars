0276be4dda5fb00c8af41476280646d61fd4ef3a
Justin Ochoa <just.och92@gmail.com>
2021-02-26 20:43:35 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="header">
+      <h1>Working with Scrolling and Hover effects</h1>
+      <div className="container">
+        <h3>1</h3>
+        <h3>2</h3>
+        <h3>3</h3>
+        <h3>4</h3>
+        <h3>5</h3>
+        <h3>6</h3>
+        <h3>7</h3>
+        <h3>8</h3>
+        <h3>9</h3>
+        <h3>10</h3>
+        <h3>11</h3>
+        <h3>12</h3>
+        <h3>13</h3>
+        <h3>14</h3>
+        <h3>15</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
